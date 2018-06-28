709d21671a1e58235680387eaae004cc71cc909f
jordiprats <jordi.prats@gmail.com>
2018-06-28 22:08:56 +0200

    lint

--- eypmanger/database/migrations/2014_10_12_000000_create_users_table.php
+++ eypmanger/database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,34 +6,34 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name')->nullable();
-            $table->string('email')->unique();
-            $table->string('nickname');
-            $table->string('password')->nullable();
-            $table->boolean('organization')->default(false);
-            $table->boolean('webhook')->default(false);
-            $table->string('webhook_password')->nullable();
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('name')->nullable();
+      $table->string('email')->unique();
+      $table->string('nickname');
+      $table->string('password')->nullable();
+      $table->boolean('organization')->default(false);
+      $table->boolean('webhook')->default(false);
+      $table->string('webhook_password')->nullable();
+      $table->rememberToken();
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('users');
+  }
 }
--- eypmanger/database/migrations/2018_06_27_200336_create_releases_table.php
+++ eypmanger/database/migrations/2018_06_27_200336_create_releases_table.php
@@ -6,31 +6,31 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateReleasesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('releases', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('release_name');
-            $table->string('branch')->default('master');
-            $table->integer('repo_id')->references('id')->on('repos');
-            $table->boolean('latest')->default(false);
-            $table->boolean('prerelease')->default(false);
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('releases', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('release_name');
+      $table->string('branch')->default('master');
+      $table->integer('repo_id')->references('id')->on('repos');
+      $table->boolean('latest')->default(false);
+      $table->boolean('prerelease')->default(false);
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('releases');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('releases');
+  }
 }
