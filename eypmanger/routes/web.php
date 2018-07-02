7c40e6e6eb2b8fce84de0e485dbf372364a59fa5
jordiprats <jordi.prats@gmail.com>
2018-07-02 23:54:44 +0200

    fulles estil

--- eypmanger/routes/web.php
+++ eypmanger/routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('/login/{provider}',          'Auth\SocialAccountController@redirectToProvider');
 Route::get('/login/{provider}/callback', 'Auth\SocialAccountController@handleProviderCallback');
