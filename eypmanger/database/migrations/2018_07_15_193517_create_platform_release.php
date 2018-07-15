40a55d81a5b595e7b4a2f78779a9dcff7026887b
jordiprats <jordi.prats@gmail.com>
2018-07-15 21:36:40 +0200

    platform release

---
+++ eypmanger/database/migrations/2018_07_15_193517_create_platform_release.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlatformRelease extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('platform_release', function (Blueprint $table) {
+      $table->increments('id');
+      $table->integer('platform_id')->references('id')->on('platforms');
+      $table->integer('release_id')->references('id')->on('releases');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('platform_release');
+  }
+}
