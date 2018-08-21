c95839320bc2a7c7e42af2914e258d51d76b2fc5
jordiprats <jordi.prats@gmail.com>
2018-08-21 08:01:00 +0200

    default prerelease

--- eypmanger/database/migrations/2018_07_15_193517_create_platform_release.php
+++ eypmanger/database/migrations/2018_07_15_193517_create_platform_release.php
@@ -17,7 +17,7 @@ class CreatePlatformRelease extends Migration
       $table->increments('id');
       $table->integer('platform_id')->references('id')->on('platforms');
       $table->integer('release_id')->references('id')->on('releases');
-      $table->boolean('prerelease');
+      $table->boolean('prerelease')->default(false);
       $table->boolean('draft')->default(false);
       $table->timestamps();
     });
